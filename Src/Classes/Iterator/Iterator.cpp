1d91c6376e4d98b6bc00c37e08ae33c961b9c3ce
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-01-24 17:02:03 -0300

    estilo(Iterator): aproximando funcoes irmas

--- Src/Classes/Iterator/Iterator.cpp
+++ Src/Classes/Iterator/Iterator.cpp
@@ -5,5 +5,4 @@ Iterator::Iterator() = default;
 Iterator::Iterator(Node *address) : pointer(address) {}
 
 double &Iterator::operator*() { return pointer->getValue(); }
-
 const double &Iterator::operator*() const { return pointer->getValue(); }
