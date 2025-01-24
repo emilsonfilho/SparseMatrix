5a7976558a00a58442e19564894e7b820c8544f1
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-01-24 08:29:51 -0300

    func(Iterator): adicionando movimentacao para o proximo elemento na linha

--- Src/Classes/Iterator/Iterator.cpp
+++ Src/Classes/Iterator/Iterator.cpp
@@ -3,3 +3,8 @@
 Iterator::Iterator() = default;
 
 Iterator::Iterator(Node *address) : pointer(address) {}
+
+void Iterator::nextInRow() {
+  if (pointer != nullptr and pointer->getNext() != pointer)
+    pointer = pointer->getNext();
+}
