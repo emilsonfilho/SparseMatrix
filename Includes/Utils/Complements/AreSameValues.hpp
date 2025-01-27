4d3ecd086d369b546bae87cc044ddc5973554550
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-01-27 19:09:19 -0300

    docs(Utils): comentando

--- Includes/Utils/Complements/AreSameValues.hpp
+++ Includes/Utils/Complements/AreSameValues.hpp
@@ -1,6 +1,12 @@
 #ifndef ARE_SAME_VALUE_HPP
 #define ARE_SAME_VALUE_HPP
 
+/**
+ * @brief Verifica se dois valores inteiros são iguais
+ * 
+ * @param a Primeiro valor
+ * @param b Segundo valor
+ */
 bool areSameValues(int a, int b);
 
 #endif
