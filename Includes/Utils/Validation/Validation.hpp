01e1c40e9695a2bff536da28dc90885e840f2c6c
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-01-29 20:59:04 -0300

    refatoracao(Validation): separando verificacoes em partes menores

--- Includes/Utils/Validation/Validation.hpp
+++ Includes/Utils/Validation/Validation.hpp
@@ -8,17 +8,46 @@
 #include "../../Exceptions/DifferentRowsException.hpp"
 #include "../../Exceptions/InvalidColumnException.hpp"
 #include "../../Exceptions/InvalidRowException.hpp"
+#include "../../Exceptions/NoMatricesException.hpp"
 #include "../Complements/AreSameValues.hpp"
 
 // serve para a validação antes da criação da matriz
 namespace ValidationUtils {
 /**
- * @brief Faz a verificação de linha e coluna antes da criação da matrix
+ * @brief Faz a verificação da linha antes da criação da matriz
+ * 
+ * @param row Número da linha
+ */
+void verifyRow(int row);
+
+/**
+ * @brief Faz a verificação da coluna antes da criação da matriz
+ * 
+ * @param col Número da linha
+ */
+void verifyCol(int col);
+
+/**
+ * @brief Faz a verificação de linha e coluna antes da criação da matriz
  *
  * @param row Número da linha
  * @param col Número da coluna
  */
 void verifyRowCol(int row, int col);
+
+/**
+ * @brief Faz verificação se row é válido
+ * 
+ * @param row Número da linha
+ */
+void verifyValidRow(int row);
+
+/**
+ * @brief Faz verificação se col é válido
+ * 
+ * @param col Número da linha
+ */
+void verifyValidCol(int col);
 
 /**
  * @brief Faz verificação se Node é válido
@@ -53,14 +82,39 @@ void verifyDifferentCol(int col1, int col2);
 void verifyDifferentRow(int row1, int row2);
 
 /**
+ * @brief Verifica se a linha dada esá dentro dos limites da matriz
+ * 
+ * @param row Linha especificada
+ * @param numRows Quantidade de linhas na matrix
+ */
+void verifyValidRowIndex(int row, int numRows);
+
+/**
+ * @brief Verifica se a coluna dada está dentro dos limites da matriz
+ * 
+ * @param col Coluna especificada
+ * @param numCols Quantidade de counas na matriz
+ */
+void verifyValidColIndex(int col, int numCols);
+
+/**
  * @brief Verifica se os índices dados estão dentro dos limites da matriz
  * 
  * @param row Linha especificada
  * @param col Coluna especificada
- * @param numRows Quantidade de linhas na matrix
- * @param numCols Quantidade de colunas na matrix
+ * @param numRows Quantidade de linhas na matriz
+ * @param numCols Quantidade de colunas na matriz
  */
 void verifyValidIndexes(int row, int col, int numRows, int numCols);
+
+/**
+ * @brief Verifica se existem matrizes disponíveis no sistema
+ * 
+ * Se não houver, lança uma exceção
+ * 
+ * @param size Tamanho do array que guarda os ponteiros para as matrizes no sistema
+ */
+void verifyIfMatrixArrayIsEmpty(int size);
 } // namespace ValidationUtils
 
 #endif
