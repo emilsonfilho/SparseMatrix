c97f5668b605fc84bfe5edec2a78aed8f617878e
Emilson Filho <emilsonfilhocontato@gmail.com>
2025-01-29 20:41:35 -0300

    refatoracao(Defs): adicionar e simplificar tipos genericos novos

---
+++ Defs/UserQuestions.hpp
@@ -0,0 +1,10 @@
+#ifndef USER_QUESTIONS_HPP
+#define USER_QUESTIONS_HPP
+
+#include <string>
+
+const std::string AskMatrixNumber = "Digite o numero da matriz: ";
+const std::string AskRow = "Digite o numero da linha: ";
+const std::string AskCol = "Digite o numero da coluna: ";
+
+#endif
